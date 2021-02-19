1a785150915381d625ac665e0e96324930a22994
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-19 14:43:44 +0100

    third exercise

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,21 @@
 //insert your pseudocode below
 /*
 
-here
+set a variable to user text.
+create a prompt with input and put it inside user text.
+using the message type any words
+calculate numbers of characters
+print the number of characters for a given words 
+using the message given word + has + numbers + character.
+
+
+
 
 */
 
 //insert your code below
+var Menu;
+
+
+Menu = window.prompt('type what you want ');
+window.alert([Menu,' has ',Menu.length,' characters',''].join(''));
