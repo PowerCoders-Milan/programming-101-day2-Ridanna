eaaefa924af7596821db6930ddaf8ead8f2ae8c0
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-19 12:46:25 +0100

    first exercise

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
 /*
 
-here
-
+set a variable to a name
+ask the user to give their name 
+set the greeting message to hello.
+follow the message by their given name
+print say hello with their name
 */
 
 //insert your code below
+var mahmud;
+
+
+mahmud = window.prompt('give name');
+window.alert(['hello',' ',mahmud].join(''));
